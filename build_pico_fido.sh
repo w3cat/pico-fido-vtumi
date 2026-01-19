e7f1506af9f93171064d09b71df78bd2bb6f1875
w3cat <117510705+w3cat@users.noreply.github.com>
2026-01-19 15:46:35 +0800

    Update build_pico_fido.sh

--- build_pico_fido.sh
+++ build_pico_fido.sh
@@ -14,12 +14,12 @@ cd build_release
 
 PICO_SDK_PATH="${PICO_SDK_PATH:-../../pico-sdk}"
 SECURE_BOOT_PKEY="${SECURE_BOOT_PKEY:-../../ec_private_key.pem}"
-boards=("pico" "pico2")
+boards=("pico2")
 
 for board_name in "${boards[@]}"
 do
     rm -rf -- ./*
-    PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name -DSECURE_BOOT_PKEY=${SECURE_BOOT_PKEY}
+    PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name -DSECURE_BOOT_PKEY=${SECURE_BOOT_PKEY} -DENABLE_EDDSA=1 -DUSB_VID=0x1050 -DUSB_PID=0x0407
     make -j`nproc`
     mv pico_fido.uf2 ../release/pico_fido_$board_name-$SUFFIX.uf2
 done
